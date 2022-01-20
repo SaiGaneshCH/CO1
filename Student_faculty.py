bae5b1321a3f25ffae697153325e1e2db614db2c
SaiGaneshCH <90609468+SaiGaneshCH@users.noreply.github.com>
2022-01-20 07:41:54 +0530

    Initial commit

---
+++ Student_faculty.py
@@ -0,0 +1,12 @@
+student=['Ganesh','Krishna','Nandu','Poorna']
+faculty=['MM','Hari','Balaji','Vishnu']
+fs=[{
+    'MM': [student[:2]],
+    'Hari':[student[:1]],
+    'Balaji':[student[:3]],
+    'Vishnu':[student[:3]]
+}]
+x=len(student)
+y=len(faculty)
+for i in fs:
+    if (i=)
